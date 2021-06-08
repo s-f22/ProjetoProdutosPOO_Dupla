2bab2690ba4db8c667e5f1d74d121069d4f46dd3
ismot <israel.j.mota@hotmai.com>
2021-06-08 15:20:51 -0300

    iniciando marca

--- INTERFACES/IMARCA.cs
+++ INTERFACES/IMARCA.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using ProjetoProdutosPOO_Dupla.Classes;
+
 namespace ProjetoProdutosPOO_Dupla.INTERFACES
 {
     public interface IMARCA
     {
-         
+        string Cadastrar(MARCA Marca); 
+
+        List<MARCA> Listar();
+
+        string Deletar(MARCA Marca); 
     }
 }
